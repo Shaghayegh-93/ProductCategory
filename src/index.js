a2bf6eb67c2511803461fe15d941c3421a2772d0
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-10-02 23:39:51 +0330

    edit the line of importing index.module.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import styles from "./index.module.css";
+import "./index.module.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
